129fed321c5538ef674489c757edd7d1d96b87e1
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-03-15 20:26:46 +0000

    nomster 39

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_041234) do
+ActiveRecord::Schema.define(version: 2019_03_15_175316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_041234) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
   end
 
   create_table "places", force: :cascade do |t|
