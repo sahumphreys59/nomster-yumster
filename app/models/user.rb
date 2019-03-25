37c9dece6e9719d649830fa605123b52e29353f1
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-03-25 03:04:37 +0000

    fixed bug with destroying places

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many :places
-  has_many :comments
-  has_many :photos
+  has_many :places, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :photos, dependent: :destroy
 end
