3aebc376d99ce2aed838f59f7610f5fa71aa03a8
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-03-26 03:34:28 +0000

    finishing up nomster

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,6 @@ class NotificationMailer < ApplicationMailer
 	def comment_added(comment)
 		@place = comment.place
 		@place_owner = @place.user
-
 		mail(to: @place_owner.email,
 				subject: "A comment has been added to #{@place.name}")
 	end
